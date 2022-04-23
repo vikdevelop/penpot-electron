e40224d542f43272ec2c032e10e5992fe5115f6c
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-04-23 13:34:35 +0200

    Add files via upload

---
+++ src/main/main.js
@@ -0,0 +1,33 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 3840,
+    height: 2160,
+    icon: '/resources/penpot.png',
+    title: "Penpot",
+    fullscreen: false,
+    autoHideMenuBar: true,
+    transparent: 'true',
+    webPreferences: {
+      preload: path.join(__dirname, 'src/renderer/preload.js')
+    }
+  })
+  mainWindow.setMenuBarVisibility(false)
+  mainWindow.loadURL('https://design.penpot.app/#/auth/login')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
