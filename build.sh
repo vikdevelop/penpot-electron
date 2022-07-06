4e169ac0a626f86a98a7c8eb7a6bc8edd3af5011
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-06 20:28:14 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
-echo "\033[1mProject: vikdevelop/penpot-electron\033[0m"
+echo -e '\033[1m Project: vikdevelop/penpot-electron \033[0m'
 echo "- Downloading Manifest"
 cd /tmp
 wget -c https://raw.githubusercontent.com/vikdevelop/penpot-electron/main/manifest.yaml > /dev/null 2>&1
+mkdir flatpak
 echo "- Building and installing penpot-electron with flatpak builder"
 echo "--- it's gonna take a while"
-flatpak-builder manifest.yaml flatpak --install --user > /dev/null 2>&1
-echo "\033[1mDone! Penpot-electron was builded and installed successfull!\033[0m"
+flatpak-builder flatpak manifest.yaml --install --user > /dev/null 2>&1
+echo "- Cleaning build files"
+rm -rf /tmp/flatpak
+rm /tmp/manifest.yaml
+echo -e '\033[1m Done! Penpot-electron was builded and installed successfull!  \033[0m'
