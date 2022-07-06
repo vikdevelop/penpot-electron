77b224b50540b829eb7577b24160609cb85cd86d
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-06 20:31:56 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,5 +9,6 @@ echo "--- it's gonna take a while"
 flatpak-builder flatpak manifest.yaml --install --user > /dev/null 2>&1
 echo "- Cleaning build files"
 rm -rf /tmp/flatpak
+rm -rf /tmp/.flatpak-builder
 rm /tmp/manifest.yaml
 echo -e '\033[1m Done! Penpot-electron was builded and installed successfull!  \033[0m'
